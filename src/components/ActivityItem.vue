2d323af725a6abd9250ed3dfaa2cfc09adcda71e
Avinash Kumar Singh <avinashsingh06072015@gmail.com>
2020-11-18 23:05:36 +0530

    changes

---
+++ src/components/ActivityItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <article class="post">
+    <h4>{{ act.title }}</h4>
+    <div class="media">
+      <div class="media-left">
+        <p class="image is-32x32">
+          <img src="../assets/user.png">
+        </p>
+      </div>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <a href="#">Filip Jeraga</a> updated at {{ ctime }} ago &nbsp;
+          </p>
+        </div>
+      </div>
+      <div class="media-right">
+        <span>Progress bar here</span>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+    props: {
+        act: {
+            type: Object,
+            required: true
+        },
+        ctime: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
